e74a18604f140db9f246c51ba44bd2af703b380e
DanielT-Dev <danieltrusca1@outlook.com>
2024-11-16 17:12:56 +0200

    Added some documents using SQLite.

--- recipeable/recipes_app/models.py
+++ recipeable/recipes_app/models.py
@@ -8,3 +8,16 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+class Food(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.CharField(max_length=255, default='recipes_app/default_image.jpg')  # Default value
+
+    ingredients = models.TextField(blank=True, null=True)
+    cuisine_types = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
--- recipeable/recipes_app/views.py
+++ recipeable/recipes_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Recipe
+from .models import Food
 
 def index(request):
     recipes = Recipe.objects.all()  # Fetch all recipes from the database
-    return render(request, 'recipes_app/index.html', {'recipes': recipes})
+    foods = Food.objects.all()
+
+    return render(request, 'recipes_app/index.html', {
+        'recipes': recipes,
+        'foods': foods,
+    })
+
+def food_card_view(request):
+    # Retrieve food items (you can use all, filter, etc.)
+    foods = Food.objects.all()  # Or filter specific foods
+    
+    # Pass the data to the template
+    return render(request, 'recipes_app/food_card.html', {'foods': foods})
